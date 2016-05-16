b542be1052ae1b36ed04d4f114f4518d2ffa0fa2
Pia Encarnacion <piaencar@hotmail.com>
2016-05-17 00:31:26 +0800

    fixed

--- memory_check.sh
+++ memory_check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $2 }' )
-echo $TOTAL_MEMORY
+#echo $TOTAL_MEMORY
 USED_MEMORY=$( free | grep Mem: | awk '{ print $3 }' )
-echo $USED_MEMORY
+#echo $USED_MEMORY
 percentage=$(free -m | awk 'NR==2{printf "%.0f", $3*100/$2 }')
 #percentage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-echo $percentage
+#echo $percentage
 
 email=""
 c=0
@@ -20,15 +20,15 @@ while getopts e:w:c: opt; do
 	case "$opt" in
 		e)
 			email=$OPTARG
-			echo "-e used: $email"
+			#echo "-e used: $email"
 			;;
 		w)
 			w=$OPTARG
-			echo "-w used: $w"
+			#echo "-w used: $w"
 			;;
 		c)
 			c=$OPTARG
-			echo "-c used: $c"
+			#echo "-c used: $c"
 			;;
 		\?)
 			echo "unrecognized option"
@@ -36,7 +36,7 @@ while getopts e:w:c: opt; do
 			;;
 	esac
 done
-shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+shift $((OPTIND-1))
 
 if [ $w -ge $c ]
 	then
@@ -44,18 +44,17 @@ if [ $w -ge $c ]
 		exit 0
 fi
 
-#2: used memory is greater than or equal to critical threshold
-#1: used memory is greater than or equal to warning threshold but less than than critical threshold
-#0: used memory is less than warning threshold
-
 if [ $percentage -ge $c ] 
 	then
 		#send top 10 processes with process ids that use a lot of memory to specified email address
 		#subject: YYYYMMDD HH:MM memory check - critical
-		DATE=`date +%Y-%m-%d`
+		DATE=`date +%Y%m%d`
 		TIME=`date +"%H:%M"`
-		mail -s "$DATE $TIME memory check - critical" $email
-		echo "$DATE $TIME"
+		processes=$(ps -eo pmem,pid,cmd | sort -k1 -nr | head -n 10)
+		#this assumes that mail has been installed
+		echo "$processes" | mail -s "$DATE $TIME memory check - critical" $email
+		echo "$processes"
+		echo "sent an email on $DATE at $TIME"
 		exit 2
 	elif [ $percentage -ge $w ]
 		then
